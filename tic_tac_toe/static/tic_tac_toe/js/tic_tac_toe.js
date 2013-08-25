f4f1e134e23fc13ecfd2f93f8b446adbca8d4b7b
Jon Smajda <jon@smajda.com>
2013-08-25 11:44:52 -0500

    Add initial js app + template for rendering board

--- tic_tac_toe/static/tic_tac_toe/js/tic_tac_toe.js
+++ tic_tac_toe/static/tic_tac_toe/js/tic_tac_toe.js
@@ -1,3 +1,27 @@
+var Game = function($, selector) {
+    var game = {
+        wrapper: $(selector),
+    }
+
+    game.board = game.wrapper.find('table tbody');
+    game.scoreboard = game.wrapper.find('.scoreboard');
+
+    game.template = function(data) {
+        if (!game._template) {
+            game._template = Handlebars.compile($('#table-template').html())
+        }
+        return game._template(data);
+    }
+
+    game.render = function() {
+        game.board.html(game.template({}));
+    }
+    return game
+}
+
+
+
 $(function() {
-    console.log('hello');
+    window.game = Game($, '.game');
+    game.render();
 });
